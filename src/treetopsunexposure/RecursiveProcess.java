0d4b77454aca994ae737bebffa082aee5bcf346a
Daniel Vorster <VRSDAN004@myuct.ac.za>
2018-09-14 12:04:02 +0200

    Trees + Terrain import code added.

---
+++ src/treetopsunexposure/RecursiveProcess.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package treetopsunexposure;
+
+import java.util.concurrent.RecursiveAction;
+
+/**
+ *
+ * @author danielvorster
+ */
+public class RecursiveProcess extends RecursiveAction{
+
+    private 
+              
+    
+    
+    @Override
+    protected void compute() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+    }
+    
+}
