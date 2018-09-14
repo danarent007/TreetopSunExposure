20e197154df3bd775e2f1315fe1df133900f572c
Daniel Vorster <VRSDAN004@myuct.ac.za>
2018-09-14 12:25:01 +0200

    Added code to split task, then return split task as an arrList for forkJoinTask

--- src/treetopsunexposure/RecursiveProcess.java
+++ src/treetopsunexposure/RecursiveProcess.java
@@ -6,6 +6,8 @@
 package treetopsunexposure;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveAction;
 
 /**
@@ -15,21 +17,59 @@ import java.util.concurrent.RecursiveAction;
 public class RecursiveProcess extends RecursiveAction{
 
     //Defined workload var, threshold for workload cutoff
-    private String[][] workload;
+    private Float[][] workload;
     private static final int THRESHOLD = 10;
    
     //Constructor for method
-    public RecursiveProcess(String[][] workload){
+    public RecursiveProcess(Float[][] workload){
         this.workload = workload;
     }
     
     @Override
     protected void compute() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-       
-                
-                
-                
+        // Check threshold
+        if (workload.length > THRESHOLD) 
+        {
+            //Split
+            ForkJoinTask.invokeAll(splitTasks());
+        }
+        else{
+            //TODO Process
+        }                 
     }
+    
+    //Create a list of subtasks for computation
+    private List<RecursiveProcess> splitTasks(){
+        
+        List<RecursiveProcess> subtasks = new ArrayList<>();
+        
+        //Split array at half-way point
+        int workloadLength = workload.length;
+        int splitPoint = (int)Math.floor(workloadLength/2);
+        int elements = workload[0].length;
+        //Copy elements
+        Float[][] arrayOne = new Float[splitPoint][elements];
+        
+        for (int i = 0; i < splitPoint; i++) 
+        {
+            arrayOne[i] = workload[i];
+        }
+        
+        Float[][] arrayTwo = new Float[workloadLength-splitPoint][elements];
+        for (int i = splitPoint; i < workloadLength; i++) 
+        {
+            arrayTwo[i] = workload[i];
+        }
+        
+        //Add net subtasks to list
+        subtasks.add(new RecursiveProcess(arrayOne));
+        subtasks.add(new RecursiveProcess(arrayTwo));
+        
+        return subtasks;
+    }
+    
+    
+    
+    
     
 }
